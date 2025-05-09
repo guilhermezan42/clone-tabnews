71708c97e9fe45b15e38ff36cfd55af17331dab7
guilhermezan42 <guilhermeczanella@gmail.com>
2025-05-09 00:38:26 +0000

    adiciona posgres_version, max_connections, opened_connections na status page

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,23 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as SUM;");
-  console.log(result.rows);
-  response
-    .status(200)
-    .json({ chave: "Te amo pai e obrigado Deus por estar sempre comigo" });
+  const updatedAt = new Date().toISOString();
+  const postgresVersion = await database.query("SHOW server_version");
+  const parsedVersion = postgresVersion.rows[0].server_version;
+
+  const maxConnections = await database.query("SHOW max_connections");
+  const parsedMaxConnections = parseInt(maxConnections.rows[0].max_connections);
+
+  const activeConnections = await database.query(
+    "SELECT count(*) FROM pg_stat_activity WHERE state = 'active'",
+  );
+  const parsedActiveConnections = parseInt(activeConnections.rows[0].count);
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    postgres_version: parsedVersion,
+    max_connections: parsedMaxConnections,
+    opened_connections: parsedActiveConnections,
+  });
 }
 export default status;
--- tests/integrations/api/v1/status/get.test.js
+++ tests/integrations/api/v1/status/get.test.js
@@ -1,4 +1,21 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  const postgresVersion = responseBody.postgres_version;
+  expect(postgresVersion).toBeDefined();
+  expect(postgresVersion).toBe("16.8");
+
+  const maxConnections = responseBody.max_connections;
+  expect(maxConnections).toBeDefined();
+  expect(maxConnections).toBe(100);
+
+  const openedConnections = responseBody.opened_connections;
+  expect(openedConnections).toBeDefined();
 });
